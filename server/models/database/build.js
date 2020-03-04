81b8a4dec2150631d355d73d24ebb2bbe00e3a37
Hanan795 <hawawdeh95@gmail.com>
2020-03-04 18:31:17 +0200

    final-files-uploading #21

--- server/models/database/build.js
+++ server/models/database/build.js
@@ -1,9 +1,10 @@
 const fs = require("fs");
+const path = require('path')
 
 const dbConnection = require("./connection");
 
-const sql = fs.readFileSync(`${__dirname}/build.sql`).toString();
-const sql1 = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
+const sql = fs.readFileSync(path.join(__dirname,'build.sql')).toString();
+const sql1 = fs.readFileSync(path.join(__dirname,'db_build.sql')).toString();
 const runDbBuild = cb => dbConnection.query(sql, cb);
 
 runDbBuild((err, res) => {
@@ -12,6 +13,7 @@ runDbBuild((err, res) => {
     }
     return res;
 });
+
 const readDbBuild = cb => dbConnection.query(sql1, cb);
 
 readDbBuild((err, res) => {
--- server/models/database/build.sql
+++ server/models/database/build.sql
@@ -1,20 +1,19 @@
 BEGIN;
 
-    DROP TABLE IF EXISTS users
+    DROP TABLE IF EXISTS user1
     CASCADE;
 
-CREATE TABLE users
+CREATE TABLE user1
 (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    email VARCHAR 
-    (200) NOT NULL UNIQUE,
+    email VARCHAR (200) NOT NULL UNIQUE,
     password TEXT NOT NULL
 );
 
-INSERT INTO users
+INSERT INTO user1
     (name,email,password)
 VALUES
-    ('abood', 'abood@gmail.com', 'abood1997'),
-    ('duha', 'dha@gmail.com', 'duha1996');
+    ('abood', 'abood@gmail.com', '$2a$10$sQEELGhd0MiQZxwqQYqFx.68DcKb5QuniuOkCHpl6b5HUiisFwy3G'),
+    ('duha', 'dha@gmail.com', '$2a$10$nxn65.PFVXScZQ.vuNS5yuBSmzp4E/Pc3.XU4mfAv8nXjmMEONQDi');
  COMMIT;
